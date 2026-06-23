ebe2d85e96ee1544e0c845a386661be51ba46982
Psyge <masto84@gmail.com>
2026-06-23 20:27:57 +0000

    aurorahero

--- src/components/Aurorahero.jsx
+++ src/components/Aurorahero.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useTranslation from "../hooks/useTranslation";
 import { calculateAurora } from "../utils/auroraEngine";
-import "../styles/auroraHero.css"; // <-- säädä polku omaan rakenteeseesi
+
 
 /* ========================================================================
    AuroraHero  —  yhdistetty hero + ilmoitus (vain etusivulla)
