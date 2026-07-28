d462574201d14b564702a38727596006e7a0baa6
Psyge <masto84@gmail.com>
2026-07-28 11:54:16 +0000

    chatti gemini

--- src/App.js
+++ src/App.js
@@ -15,13 +15,14 @@ import MidnightSunV2 from "./components/MidnightSunV2";
 import CookieBanner from "./components/CookieBanner";
 import PlacePage from "./PlacePage";
 import Premiummodalmanager from "./components/Premiummodalmanager";
+import Auroraassistant from "./components/Auroraassistant";
 
 function App() {
   return (
     <HelmetProvider>
       <BrowserRouter>
       <Premiummodalmanager />
-       
+      <Auroraassistant />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/map" element={<MapPage />} />
