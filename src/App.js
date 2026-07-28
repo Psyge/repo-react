40746c121f26c8dc1284a6aefcaf112dd742b7de
Psyge <masto84@gmail.com>
2026-07-28 09:48:21 +0000

    chatti

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import MidnightSunV2 from "./components/MidnightSunV2";
 import CookieBanner from "./components/CookieBanner";
 import PlacePage from "./PlacePage";
 import Premiummodalmanager from "./components/Premiummodalmanager";
-import Auroraassistant from "./components/Auroraassistant";
 
 function App() {
   return (
